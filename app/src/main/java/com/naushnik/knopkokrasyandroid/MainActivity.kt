68a211ea9b3c61f6486b4e8c7406b70373c47d55
NaushnikTapochkevich <64918486+NaushnikTapochkevich@users.noreply.github.com>
2022-04-05 02:33:29 +0300

    second commit

--- app/src/main/java/com/naushnik/knopkokrasyandroid/MainActivity.kt
+++ app/src/main/java/com/naushnik/knopkokrasyandroid/MainActivity.kt
@@ -3,8 +3,10 @@ package com.naushnik.knopkokrasyandroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -17,19 +19,23 @@ class MainActivity : AppCompatActivity() {
         val username: EditText = findViewById(R.id.edit_user)
         val password: EditText = findViewById(R.id.edit_password)
         val btnAuthorization: Button = findViewById(R.id.button_login)
-
-
-        val intent = Intent(this@MainActivity, SecondActivity::class.java)
-
+        val textError: TextView = findViewById(R.id.text)
 
         btnAuthorization.setOnClickListener {
+
+            val intent = Intent(this, SecondActivity::class.java)
+
             if ((username.length() >= 3) && (password.length() >= 8)) {
+                textError.visibility = View.INVISIBLE
+                intent.putExtra("transmitted", username.text.toString())
                 startActivity(intent)
-            } else {
-                Toast.makeText(applicationContext, "error", Toast.LENGTH_LONG).show()
+            } else if (username.length() < 3) {
+                textError.visibility = View.VISIBLE
+                textError.setText("Че то не так с логином")
+            } else if (password.length() < 8) {
+                textError.visibility = View.VISIBLE
+                textError.setText("Че то не так с паролем")
             }
         }
-        val transmitted = username.text
-        intent.putExtra("transmitted", transmitted)
     }
 }
---
+++ app/src/main/java/com/naushnik/knopkokrasyandroid/SecondActivity.kt
@@ -0,0 +1,17 @@
+package com.naushnik.knopkokrasyandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_activity)
+
+        val textView: TextView = findViewById(R.id.text_second)
+
+        val transmitted = intent.getStringExtra("transmitted")
+        textView.text = transmitted
+    }
+}
